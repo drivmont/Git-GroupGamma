6af01d0eabb03f692e543b77493b5672073e00fd
drivmont <rivmont.diego@gmail.com>
2021-12-18 21:48:07 +0000

    Script de DNA-Centerv2

--- dna-center.py
+++ dna-center.py
@@ -1,18 +1,15 @@
 import requests
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import json
 import sys
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 dnacip = "sandboxdnac.cisco.com"
 username = "devnetuser"
 password = "Cisco123!"
 
-
 def get_X_auth_token(dnacip,username,password):
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
     post_uri = "https://"+dnacip+"/dna/system/api/v1/auth/token"
-    print("\nAuthenticate: POST %s"%(post_uri))
-
     try:
         r = requests.post(post_uri, auth=(username, password), verify=False)
         return r.json()["Token"]
@@ -21,5 +18,35 @@ def get_X_auth_token(dnacip,username,password):
         print ("Response: %s"%r.text)
         sys.exit()
 
+def get_network_device(dnacip, headers, params, modifier):
+    uri = "https://"+dnacip+"/dna/intent/api/v1/network-device"+modifier
+    try:
+        resp = requests.get(uri,headers=headers,params=params,verify = False)
+        return resp
+    except:
+        print("Status: %s"%r.status_code)
+        print("Response: %s"%r.text)
+        sys.exit()
+
 token = get_X_auth_token(dnacip, username, password)
-print ("returned Authentication Token: ", (token))
+
+headers = {"x-auth-token": token}
+params=""
+modifier="/count"
+count = get_network_device(dnacip, headers, params, modifier)
+count = json.dumps(count.json()["response"])
+params=""
+modifier=""
+devices = get_network_device(dnacip, headers, params, modifier)
+devices = devices.json()
+print("DNA Center:")
+print("-------------------------------------------------")
+print("Cantidad de dispositivos: " + count)
+print("-------------------------------------------------\n")
+c = 1
+for device in devices["response"]:
+    print("Dispositivo " + str(c))
+    print(" - Nombre del dispositivo: " + device["hostname"])
+    print(" - Direccion IP de gestion: " + device["managementIpAddress"])
+    print(" - Numero de serie: " + device["serialNumber"]+"\n")
+    c += 1
